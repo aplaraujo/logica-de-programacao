357b3ee3d5c16e06a9fae9c821a9b9246a8fbe84
aplaraujo <aplaraujo@gmail.com>
2023-03-06 21:26:14 -0300

    Correção exercício A

--- programacao-com-sequencia/exercicio-a.js
+++ programacao-com-sequencia/exercicio-a.js
@@ -4,8 +4,8 @@ Fahrenheit. A fórmula de conversão é F <- C * (9 / 5) + 32, sendo F a
 temperatura em Fahrenheit e C a temperatura em Celsius.
 */
 
-const getCelsiusTemperature = tempC => {
+const getFahrenheitTemperature = tempC => {
 	return tempC * (9 / 5) + 32
 }
 
-console.log(`Temperatura em Fahrenheit: ${getCelsiusTemperature(35.5).toFixed(1)}Fº`)
+console.log(`Temperatura em Fahrenheit: ${getFahrenheitTemperature(35.5).toFixed(1)}ºF`)
