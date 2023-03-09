103cac121dbdce9d81f5b56c1427cfe5f8aeccd8
aplaraujo <aplaraujo@gmail.com>
2023-03-09 20:54:12 -0300

    Exercício D

---
+++ programacao-com-sequencia/exercicio-d.js
@@ -0,0 +1,10 @@
+/*
+Efetuar o cálculo da quantidade de litros de combustível gastos em uma 
+viagem utilizando um automóvel que faz 12 Km por litro. 
+Para obter o cálculo, o usuário deverá fornecer o tempo gasto e a 
+velocidade média durante a viagem. Desta forma, será possível obter a distância 
+percorrida com a fórmula DISTÂNCIA <- TEMPO * VELOCIDADE. Tendo o valor da distância, 
+basta calcular a quantidade de litros de combustível utilizada na viagem com a fórmula:
+LITROS_USADOS <- DISTÂNCIA / 12. O programa deverá apresentar os valores da velocidade 
+média, tempo gasto na viagem, a distância percorrida e a quantidade de litros usada na viagem.
+*/
