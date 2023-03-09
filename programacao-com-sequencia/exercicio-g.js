9a88f7561afa75384c1a197cd3de611cf56b7e91
aplaraujo <aplaraujo@gmail.com>
2023-03-09 20:57:54 -0300

    Exercício G

---
+++ programacao-com-sequencia/exercicio-g.js
@@ -0,0 +1,10 @@
+/*
+Ler quatro valores numéricos inteiros e apresentar o resultado das adições 
+e das multiplicações usando o conceito de propriedade distributiva para a 
+máxima combinação possível entre as quatro variáveis. Considerando o uso das 
+variáveis A, B, C e D, deverão ser efetuadas seis adições e seis multiplicações, 
+ou seja, de forma geral deverá ser combinada a variável A com a variável B, 
+a variável A com a variável C e a variável A com a variável D. Depois será necessário 
+combinar a variável B com a variável C e a variável B com a variável D. 
+Por fim, a variável C será combinada com a variável D.
+*/
