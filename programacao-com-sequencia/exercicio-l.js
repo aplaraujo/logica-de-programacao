a388bf89304cd58f8d09e0d7c466578abdb75614
aplaraujo <aplaraujo@gmail.com>
2023-03-09 21:04:32 -0300

    Exercício L

---
+++ programacao-com-sequencia/exercicio-l.js
@@ -0,0 +1,5 @@
+/*
+Elaborar um programa que efetue a apresentação do valor da conversão em dólar (US$) 
+de um valor lido em real (R$). O programa deverá solicitar o valor da cotação do dólar 
+e também a quantidade de reais disponível com o usuário.
+*/
