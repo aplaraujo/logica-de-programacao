e8732f11c803532a231699c3f7dcd12bbcd0abc9
aplaraujo <aplaraujo@gmail.com>
2023-03-09 21:15:36 -0300

    Exercício U

---
+++ programacao-com-sequencia/exercicio-u.js
@@ -0,0 +1,6 @@
+/*
+Construir um programa que calcule, armazene e apresente, em metros por segundo, 
+o valor da velocidade de um projétil que percorre uma distância em quilômetros 
+a um espaço de tempo em minutos. Utilize a fórmula:
+VELOCIDADE <- (DISTANCIA * 1000) / (TEMPO * 60)
+*/
