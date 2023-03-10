fd3b95fa945d92c33850dc1718e122557c59ca08
aplaraujo <aplaraujo@gmail.com>
2023-03-09 21:25:08 -0300

    Exercício X

---
+++ programacao-com-sequencia/exercicio-x.js
@@ -0,0 +1,5 @@
+/*
+Elaborar um programa que leia uma medida em pés, calcular, armazenar e 
+apresentar o seu valor convertido em metros, lembrando que um pé mede 0.3048 metro, 
+ou seja, um pé é igual a 30.48 centímetros.
+*/
