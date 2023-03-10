8c1a4fac71fba593a5c39d0ff6ee5867a48cf292
aplaraujo <aplaraujo@gmail.com>
2023-03-09 21:06:41 -0300

    Exercício N

---
+++ programacao-com-sequencia/exercicio-n.js
@@ -0,0 +1,5 @@
+/*
+Elaborar um programa que efetue a leitura de três valores inteiros 
+(representados pelas variáveis A, B e C) e apresente como resultado final 
+o valor do quadrado da soma dos três valores lidos.
+*/
