b6e9faa502dfd959bce124660c3a86168e5183ce
aplaraujo <aplaraujo@gmail.com>
2023-03-10 17:47:21 -0300

    Exercício D

--- programacao-com-sequencia/exercicio-d.js
+++ programacao-com-sequencia/exercicio-d.js
@@ -8,3 +8,13 @@ basta calcular a quantidade de litros de combustível utilizada na viagem com a 
 LITROS_USADOS <- DISTÂNCIA / 12. O programa deverá apresentar os valores da velocidade 
 média, tempo gasto na viagem, a distância percorrida e a quantidade de litros usada na viagem.
 */
+
+
+const time = 3
+const speed = 80
+
+const distance = time * speed
+
+const litersGasoline = distance / 12
+
+console.log(time, speed, distance, litersGasoline)
