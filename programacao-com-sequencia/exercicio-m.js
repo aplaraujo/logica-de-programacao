aa1d25e2544c681721c28645848f038b7202515c
aplaraujo <aplaraujo@gmail.com>
2023-03-09 21:05:39 -0300

    Exercício M

---
+++ programacao-com-sequencia/exercicio-m.js
@@ -0,0 +1,5 @@
+/*
+Elaborar um programa que efetue a leitura de três valores inteiros 
+(representados pelas variáveis A, B e C) e apresente como resultado final o 
+valor da soma dos quadrados dos três valores lidos.
+*/
