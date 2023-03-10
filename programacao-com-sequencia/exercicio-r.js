9f6ef2c6a83a50c2930dac46505047620b90564e
aplaraujo <aplaraujo@gmail.com>
2023-03-09 21:10:57 -0300

    Exercício R

---
+++ programacao-com-sequencia/exercicio-r.js
@@ -0,0 +1,5 @@
+/*
+Elaborar um programa de computador que calcule e apresente o valor da área 
+de uma circunferência (variável A). Para tanto, o programa deve solicitar o valor 
+do raio (variável R) e fazer uso da fórmula de cálculo: A <- 3.14159 * R ** 2.
+*/
