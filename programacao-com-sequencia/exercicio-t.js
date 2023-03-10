c09d8bd2ae64f5fe939a48452eeba637c6255638
aplaraujo <aplaraujo@gmail.com>
2023-03-09 21:14:29 -0300

    Exercício T

---
+++ programacao-com-sequencia/exercicio-t.js
@@ -0,0 +1,5 @@
+/*
+Elaborar um programa que leia dois valores numéricos reais desconhecidos 
+representados pelas variáveis A e B. Calcular, armazenar e apresentar os 
+resultados das quatros operações aritméticas básicas.
+*/
