0592f62a6777d21a490d9a9fffc970e37903a75e
aplaraujo <aplaraujo@gmail.com>
2023-03-09 21:03:23 -0300

    Exercício K

---
+++ programacao-com-sequencia/exercicio-k.js
@@ -0,0 +1,5 @@
+/*
+Elaborar um programa que efetue a apresentação do valor da conversão em real (R$) 
+de um valor lido em dólar (US$). O programa deverá solicitar o valor da cotação do dólar 
+e também a quantidade de dólares disponível com o usuário.
+*/
