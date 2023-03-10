dc6605397431b0bddfee4e363417f607d5d372cc
aplaraujo <aplaraujo@gmail.com>
2023-03-09 21:09:40 -0300

    Exercício P

---
+++ programacao-com-sequencia/exercicio-p.js
@@ -0,0 +1,5 @@
+/*
+Ler o valor correspondente ao salário mensal (variável SM) 
+de um trabalhador e também o valor percentual de reajuste (variável PR) 
+a ser atribuído. Apresentar o valor do novo salário (variável NS).
+*/
