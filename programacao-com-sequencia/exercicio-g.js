cdc3e186c707890613b97991c29b872fcb2e2412
aplaraujo <aplaraujo@gmail.com>
2023-03-10 18:01:07 -0300

    Exercício G

--- programacao-com-sequencia/exercicio-g.js
+++ programacao-com-sequencia/exercicio-g.js
@@ -8,3 +8,25 @@ a variável A com a variável C e a variável A com a variável D. Depois será 
 combinar a variável B com a variável C e a variável B com a variável D. 
 Por fim, a variável C será combinada com a variável D.
 */
+
+const A = 15
+const B = 5
+const C = 4
+const D = 2
+
+const s1 = A + B
+const s2 = A + C
+const s3 = A + D
+const s4 = B + C
+const s5 = B + D
+const s6 = C + D
+
+const m1 = A * B
+const m2 = A * C
+const m3 = A * D
+const m4 = B * C
+const m5 = B * D
+const m6 = C * D
+
+console.log(s1, s2, s3, s4, s5, s6)
+console.log(m1, m2, m3, m4, m5, m6)
