3cc35d0d39d35dcca1474b4b3e7e98583b474888
aplaraujo <aplaraujo@gmail.com>
2023-03-10 18:38:50 -0300

    Exercício P

--- programacao-com-sequencia/exercicio-p.js
+++ programacao-com-sequencia/exercicio-p.js
@@ -3,3 +3,11 @@ Ler o valor correspondente ao salário mensal (variável SM)
 de um trabalhador e também o valor percentual de reajuste (variável PR) 
 a ser atribuído. Apresentar o valor do novo salário (variável NS).
 */
+
+const newSalary = (salary, raise) => {
+    return salary + (salary * (raise / 100))
+}
+
+console.log(newSalary(2000, 4).toFixed(2))
+console.log(newSalary(2500, 10).toFixed(2))
+console.log(newSalary(1000, 5).toFixed(2))
