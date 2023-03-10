ef37041515e6f013fc08ff358a36f477a84a4594
aplaraujo <aplaraujo@gmail.com>
2023-03-10 18:18:31 -0300

    Exercício K

--- programacao-com-sequencia/exercicio-k.js
+++ programacao-com-sequencia/exercicio-k.js
@@ -3,3 +3,12 @@ Elaborar um programa que efetue a apresentação do valor da conversão em real 
 de um valor lido em dólar (US$). O programa deverá solicitar o valor da cotação do dólar 
 e também a quantidade de dólares disponível com o usuário.
 */
+
+const dollarToBrazilianReal = (dollar, exchangeRate) => {
+    return dollar * exchangeRate
+}
+
+console.log(dollarToBrazilianReal(500, 5.30))
+console.log(dollarToBrazilianReal(100, 5.30))
+console.log(dollarToBrazilianReal(1000, 5.30))
+console.log(dollarToBrazilianReal(50, 5.30))
