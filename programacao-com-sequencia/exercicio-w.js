18b1860b5ba533da183fcc944ec170b4b8bbbd6e
aplaraujo <aplaraujo@gmail.com>
2023-03-09 21:23:44 -0300

    Exercício W

---
+++ programacao-com-sequencia/exercicio-w.js
@@ -0,0 +1,5 @@
+/*
+Elaborar um programa que leia dois valores numéricos inteiros, os quais devem representar a 
+base e o expoente de uma potência. Calcular a potência, armazenar em memória o resultado 
+calculado e apresentar o resultado obtido.
+*/
