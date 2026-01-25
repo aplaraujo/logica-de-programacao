d8af690c602ddca5a795d5d46042f2581e598249
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-01-25 10:40:33 -0300

    Exemplo 1

---
+++ java/algoritmos-livro/capitulo-3/exemplos/exemplo-1/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A, B, X;
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+        X = A + B;
+
+        System.out.println("Resultado da soma: " + X);
+
+        sc.close();
+    }
+}
