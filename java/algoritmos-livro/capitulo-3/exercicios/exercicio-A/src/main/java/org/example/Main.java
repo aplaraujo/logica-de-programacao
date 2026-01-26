7668890e41d75abc05e3c53d47b96798252ebe0b
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-01-26 20:33:57 -0300

    Exercício A

---
+++ java/algoritmos-livro/capitulo-3/exercicios/exercicio-A/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double C, F;
+        System.out.print("Digite a temperatura em graus Celsius: ");
+        C = sc.nextDouble();
+        F = (C * (9.0 / 5.0)) + 32;
+
+        System.out.printf("Temperatura em graus Fahrenheit: %.1f", F);
+    }
+}
