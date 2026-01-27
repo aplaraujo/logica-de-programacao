26193b73f9c472b32014a8a9f459decd80ebd5bf
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-01-26 21:09:28 -0300

    Exercício E

--- java/algoritmos-livro/capitulo-3/exercicios/exercicio-E/src/main/java/Main.java
+++ java/algoritmos-livro/capitulo-3/exercicios/exercicio-E/src/main/java/Main.java
@@ -1,2 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int taxa, tempo;
+        double valor, prestacao;
+
+        System.out.print("Digite o valor do produto: ");
+        valor = sc.nextDouble();
+        System.out.print("Digite a taxa em atraso: ");
+        taxa = sc.nextInt();
+        System.out.print("Digite o tempo: ");
+        tempo = sc.nextInt();
+
+        prestacao = valor + (valor * (taxa / 100.0) * tempo);
+
+        System.out.println("Valor da prestação: R$ " + String.format("%.2f", prestacao));
+
+        sc.close();
+    }
 }
