ac4f237fe463f677c150affa7e620b57aab6b276
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-01-26 21:54:38 -0300

    Exercício K

---
+++ java/algoritmos-livro/capitulo-3/exercicios/exercicio-K/src/main/java/Main.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite a quantidade de dólares: ");
+        double dolar = sc.nextDouble();
+        System.out.print("Digite o valor da cotação: ");
+        double cotacao = sc.nextDouble();
+
+        double real = dolar * cotacao;
+
+        System.out.println("Valor em reais: R$ " + String.format("%.2f", real));
+
+        sc.close();
+    }
+}
