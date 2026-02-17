a82b4e1ab4f4e7347a6a5a1a28f23d57d998e733
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-02-17 09:28:29 -0300

    Capítulo 5 - Exercício A1

---
+++ java/algoritmos-livro/capitulo-5/exercicios/exercicio-A1/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 15;
+
+        while(i <= 200) {
+            System.out.println("O quadrado de " + i + " é: " + i * i);
+            i++;
+        }
+
+    }
+}
