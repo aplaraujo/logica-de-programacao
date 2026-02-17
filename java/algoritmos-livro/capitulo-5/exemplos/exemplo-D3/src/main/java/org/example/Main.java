0ce6061e92c399ce59932c5f573a666c8edb285a
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-02-17 09:04:47 -0300

    Capítulo 5 - Exemplo D3

---
+++ java/algoritmos-livro/capitulo-5/exemplos/exemplo-D3/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i;
+
+        for (i = 0; i <= 10 ; i++) {
+            System.out.println(i);
+        }
+
+        sc.close();
+    }
+}
