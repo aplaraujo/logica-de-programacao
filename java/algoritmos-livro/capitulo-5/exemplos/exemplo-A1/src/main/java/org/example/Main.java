394180d43358b6498ecb968c42f4fbe7ad5a42d0
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-02-17 08:31:33 -0300

    Capítulo 5 - Exemplo A1

---
+++ java/algoritmos-livro/capitulo-5/exemplos/exemplo-A1/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i, num, resultado;
+        i = 1;
+
+        while (i <= 5) {
+            System.out.print("Digite um número: ");
+            num = sc.nextInt();
+
+            resultado = num * 3;
+            System.out.println("Resultado: " + resultado);
+            i++;
+        }
+
+        sc.close();
+    }
+}
