9803fa4879d50dd103201cb3f7722d0c11bbfd5d
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-02-17 07:55:00 -0300

    Capítulo 4 - Exercício L

---
+++ java/algoritmos-livro/capitulo-4/exercicios/exercicio-L/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        if (!(num > 3)) {
+            System.out.println("Valor digitado: " + num);
+        }
+
+        sc.close();
+    }
+}
