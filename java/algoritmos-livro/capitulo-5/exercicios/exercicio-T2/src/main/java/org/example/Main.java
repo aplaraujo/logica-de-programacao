364964ec7d37112056c5d9a663484e6e3e1093d6
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-02-19 09:13:06 -0300

    Capítulo 5 - Exercício T2

---
+++ java/algoritmos-livro/capitulo-5/exercicios/exercicio-T2/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 15; i <= 200; i += 3) {
+            System.out.println(i * i);
+        }
+    }
+}
