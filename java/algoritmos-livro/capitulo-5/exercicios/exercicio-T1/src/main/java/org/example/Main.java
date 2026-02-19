221c28e67f3691893623d4d16b43bb21316072cc
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-02-19 09:10:30 -0300

    Capítulo 5 - Exercício T1

---
+++ java/algoritmos-livro/capitulo-5/exercicios/exercicio-T1/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int i;
+        i = 15;
+
+        while(i <= 200) {
+            System.out.println(i*i);
+            i+= 3;
+        }
+    }
+}
