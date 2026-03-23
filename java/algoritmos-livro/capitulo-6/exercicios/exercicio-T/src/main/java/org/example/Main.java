7a4c982e799818e7051324b4e9e06b74daf899db
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-03-23 15:25:26 -0300

    Capítulo 6 - Exercício T

--- java/algoritmos-livro/capitulo-6/exercicios/exercicio-T/src/main/java/org/example/Main.java
+++ java/algoritmos-livro/capitulo-6/exercicios/exercicio-T/src/main/java/org/example/Main.java
@@ -9,19 +9,19 @@ public class Main {
         int i, j, num;
         i = 0;
 
-        // Criar o vetor grupoA com 6 elementos (números inteiros)
-        int[] grupoA = new int[6];
+        // Criar o vetor grupoA com 10 elementos (números inteiros)
+        int[] grupoA = new int[10];
 
-        // Criar o vetor grupoB com 6 elementos (números inteiros)
-        int[] grupoB = new int[6];
+        // Criar o vetor grupoB com 10 elementos (números inteiros)
+        int[] grupoB = new int[10];
 
-        // Criar o vetor grupoC com 12 elementos (números inteiros)
-        int[] grupoC = new int[12];
+        // Criar o vetor grupoC com 20 elementos (números inteiros)
+        int[] grupoC = new int[20];
 
         j = 0;
 
         // Ler os valores no vetor grupoA (apenas números divisíveis por 2 e 3, validados pelo programa)
-        while(i < 6) {
+        while(i < 10) {
             System.out.print("Digite um número inteiro divisível por 2 e 3: ");
             num = sc.nextInt();
 
@@ -36,7 +36,7 @@ public class Main {
         System.out.println("===========================");
 
         // Ler os valores ne vetor grupoB (apenas números divisíveis por 5, validados pelo programa)
-        while(j < 6) {
+        while(j < 10) {
             System.out.print("Digite um número inteiro divisível por 5: ");
             num = sc.nextInt();
 
@@ -49,19 +49,19 @@ public class Main {
         }
 
         // Incluir no vetor grupoC os valores dos vetores grupoA e grupoB
-        for (i = 0; i < 6; i++) {
+        for (i = 0; i < 10; i++) {
             grupoC[i] = grupoA[i];
         }
 
-        for (i = 6; i < 12; i++) {
-            grupoC[i] = grupoB[i - 6];
+        for (i = 10; i < 20; i++) {
+            grupoC[i] = grupoB[i - 10];
         }
 
         System.out.println("============================");
         System.out.println("======== Vetor C ===========");
 
         // Mostrar o vetor grupoC
-        for (i = 0; i < 12; i++) {
+        for (i = 0; i < 20; i++) {
             System.out.println(grupoC[i]);
         }
 
