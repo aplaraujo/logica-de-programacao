7e616848f8da64d928e2c190b33aa29ade896702
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-04-22 07:49:13 -0300

    Capítulo 7 - Exercício S

--- java/algoritmos-livro/capitulo-7/exercicios/exercicio-S/src/main/java/org/example/Main.java
+++ java/algoritmos-livro/capitulo-7/exercicios/exercicio-S/src/main/java/org/example/Main.java
@@ -1,7 +1,46 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int i, j, pesquisa;
+        String resposta;
+        boolean achou;
+
+        int[] grupoA = new int[10];
+
+        for (i = 0; i < 10; i++) {
+            System.out.print("A[" + i + "]: ");
+            grupoA[i] = sc.nextInt();
+        }
+
+        resposta = "s";
+
+        while (resposta.equals("s") || resposta.equals("S")) {
+            System.out.print("Digite um número para pesquisa: ");
+            pesquisa = sc.nextInt();
+
+            i = 0;
+            achou = false;
+            while ((i < 10) && (!achou)) {
+                if (pesquisa == grupoA[i]) {
+                    achou = true;
+                } else {
+                    i++;
+                }
+            }
+            if (achou) {
+                System.out.println(pesquisa + " foi encontrado na posição " + i);
+            } else {
+                System.out.println(pesquisa + " não foi encontrado!");
+            }
+            System.out.print("Deseja continuar? ");
+            resposta = sc.next();
+        }
+
+        sc.close();
     }
 }
